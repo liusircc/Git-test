0fc334d567fc0f5845ef0c7db3eb8ecdc337b8cc
xiaojian <xiaojian@163.com>
2022-02-15 10:51:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author liusir
+ * @create 2022-02-15  10:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
